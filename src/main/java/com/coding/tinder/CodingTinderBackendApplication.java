9f0a6bf47e0b7c2096f82739afb13d0d673a19f6
samuel-lee-sea <samuellee9508@gmail.com>
2024-09-07 17:48:06 -0700

    Initial commit

---
+++ src/main/java/com/coding/tinder/CodingTinderBackendApplication.java
@@ -0,0 +1,13 @@
+package com.coding.tinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingTinderBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodingTinderBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coding/tinder/CodingTinderBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coding.tinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodingTinderBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
